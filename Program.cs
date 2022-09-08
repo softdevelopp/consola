fae113084c939ab84b6c8ad958583de02d6818b7
softdevelopp <softdevelopp@gmail.com>
2022-09-07 20:44:47 -0500

    second commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("MisionTic Ucaldas 2022");
+Console.WriteLine("MisionTic Ucaldas 2022 G40");
+Console.WriteLine("MisionTic 2022 G40 C3");
